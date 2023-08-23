9aa89b71496c51aaacc57fec5f72fcb6eb2beb27
Windy Sabtami <windysabtami92@gmail.com>
2023-08-23 20:50:05 +0800

    bisa

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,15 +3,27 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
+
+    public static $permissions = [
+        'dashboard' => [
+            'Superadmin', 'admin',
+        ],
+        'user' => ['admin']
+    ];
     /**
      * The model to policy mappings for the application.
      *
      * @var array<class-string, class-string>
      */
+
+
     protected $policies = [
         //
     ];
@@ -21,6 +33,23 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // foreach (self::$permissions as $key => $value) {
+        //     Gate::define($key, function (User $user) use ($value) {
+        //         return in_array($user->role, $value);
+        //     });
+        // }
+        foreach (self::$permissions as $feature => $roles) {
+            Gate::define($feature, function (User $user) use ($roles) {
+                if (in_array($user->role, $roles)) {
+                    return true;
+                }
+            });
+        }
+
+        // Gate::define('dashboard', function (User $user) {
+        //     if ($user->role == 'Superadmin') {
+        //         return true;
+        //     }
+        // });
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,11 +14,25 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
+        User::factory(20)->create();
         User::create([
             'name' => 'Windy Sabtami',
             'email' => 'windysabtami85@gmail.com',
             'email_verified_at'  => now(),
+            'role' => 'admin',
+            'phone' => '08123456789',
+            'bio' => 'IT Developer',
+
+            'password' => Hash::make('12345'),
+        ]);
+        User::create([
+            'name' => 'Azka Jihan',
+            'email' => 'azkajihan92@gmail.com',
+            'email_verified_at'  => now(),
+            'role' => 'Superadmin',
+            'phone' => '08123456789',
+            'bio' => 'IT Engineer',
+
             'password' => Hash::make('12345'),
         ]);
     }
