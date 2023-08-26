82f39e53f02ce265df146164b19fa3fb932f3320
Windy Sabtami <windysabtami92@gmail.com>
2023-08-26 18:45:44 +0800

    auth register logout login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,43 +11,64 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 
+
 class AuthController extends Controller
 {
-//     public function __construct()
-//     {
-//         $this->middleware('auth:api', ['except' => ['login']]);
-//     }
-
     public function login(Request $request)
     {
         $request->validate([
-           'email' => 'required|string|email',
-           'password' => 'required', 
+            'email' => 'required|string|email',
+            'password' => 'required'
         ]);
 
         $user = User::where('email', $request->email)->first();
 
-        if(!$user){
+        if (!$user) {
             throw ValidationException::withMessages([
-                'email' => ['email tidak ditemukan'],
-            ]);
-        }
-        if(!Hash::check($request->password, $user->password)){
-            throw ValidationException::withMessages([
-                'password' => ['password salah'],
+                'email' => ['email incorrect']
             ]);
         }
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+        if (!Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => ['password incorrect']
+            ]);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
         return response()->json([
             'jwt-token' => $token,
             'user' => new UserResource($user),
         ]);
-
-
     }
 
-    public function logout(Request $request){
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
 
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user',
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+        return response()->json([
+            'jwt-token' => $token,
+            'user' => new UserResource($user),
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'logout successfully',
+        ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'email',
         'password',
         'bio',
-        'phone'
+        'phone',
+        'role'
     ];
 
     /**
